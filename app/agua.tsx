a889d070e479e3d437a0e11b4722d1527d4d9f04
BrunoSossella <bdcsossella@minha.fag.edu.br>
2025-05-20 21:24:08 -0300

    Commit inicial com autor corrigido

---
+++ app/agua.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function AguaScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Tela de Registro de Água</Text>
+    </View>
+  );
+}
---
+++ app/historico.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HistoricoScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Histórico de Saúde</Text>
+    </View>
+  );
+}
